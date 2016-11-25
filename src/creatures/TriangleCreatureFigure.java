e77daf5a04034ee49d051e8226c12ba709854f16
Karonsbo <karro.jonzen@hotmail.com>
2016-11-25 15:07:31 +0100

    Creatures update

--- src/creatures/TriangleCreatureFigure.java
+++ src/creatures/TriangleCreatureFigure.java
@@ -1,7 +1,11 @@
 package creatures;
 
+import java.awt.Graphics2D;
+
 import start.Figures;
 import start.Position;
+import towers.TowerFigure;
+import utilities.CustomShapes;
 
 public class TriangleCreatureFigure extends CreatureFigure{
 
@@ -12,11 +16,36 @@ public class TriangleCreatureFigure extends CreatureFigure{
 	}
 
 	@Override
-	public void update(){
+	public void update() {
+		if (this.hasSpawned == 0) {
+			for (Action action : this.onSpawnActionList) {
+				action.executeAction();
+			}
+			this.hasSpawned++;
+		}
+
+		for (Action action : this.onActiveActionList) {
+			action.executeAction();
+		}
 	}
 
+	public void moveForward() {
+		if (this.isAlive()) {
+			Position currentPosition = this.getPosition();
+
+			Position newPosition = new Position(currentPosition.getX() + 1,
+					currentPosition.getY() + 1);
+
+			this.setPosition(newPosition);
+		}
+	}
+
+
 	@Override
-	public void render(){
+	public void render(Graphics2D g2d){
+		g2d.setColor(this.getColor());
+		g2d.fill(CustomShapes.createTriangle(this.getPosition(),
+				CreatureFigure.TEMP_SIZE));
 	}
 
 	@Override
