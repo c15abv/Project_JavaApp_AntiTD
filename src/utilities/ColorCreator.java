8c41583eb4b26b4bd7cabba27587af303ab815d9
c15abv <c15abv@cs.umu.se>
2016-11-25 15:37:40 +0100

    Add files via upload

--- src/utilities/ColorCreator.java
+++ src/utilities/ColorCreator.java
@@ -4,72 +4,71 @@ import java.awt.Color;
 
 public class ColorCreator{
 	
-	private static final float LUMINATION = 0.5f;
-	private static final float SATURATION = 1.0f;
-	private static final float LS_MULTIPLIER = 
-			(LUMINATION + SATURATION) - LUMINATION * SATURATION;
-	private static final float LL_MULTIPLIER =
-			LUMINATION - LS_MULTIPLIER;
+	private static final double CONST = 0.33333;
+	private static final double VALUE = 0.5;
+	private static final double SATURATION = 1.0;
+	private static final double LS_MULTIPLIER = 
+			(VALUE + SATURATION) - VALUE * SATURATION;
+	private static final double LL_MULTIPLIER =
+			VALUE - LS_MULTIPLIER;
 	
 	//8-bit color generator based on given hue.
-	public static Color generateColorFromHue(int hue){
-		float hue_rad = hue / 360;
-		float red_temp = hue_rad + (float)(1/3);
-		float green_temp = hue_rad;
-		float blue_temp = hue_rad - (float)(1/3);
+	//broken...
+	public static Color generateColorFromHueBROKEN(int hue){
+		double hue_rad = (double)(hue) / (double)(360);
+		double red_temp = hue_rad + CONST;
+		double green_temp = hue_rad;
+		double blue_temp = hue_rad - CONST;
 		
-		adjustRange(red_temp);
-		adjustRange(green_temp);
-		adjustRange(blue_temp);
+		red_temp = adjustRange(red_temp);
+		green_temp = adjustRange(green_temp);
+		blue_temp = adjustRange(blue_temp);
 		
-		runCorrectionTests(red_temp);
-		runCorrectionTests(green_temp);
-		runCorrectionTests(blue_temp);
+		red_temp = runCorrectionTests(red_temp);
+		red_temp = runCorrectionTests(green_temp);
+		red_temp = runCorrectionTests(blue_temp);
 		
-		return new Color(Math.round(red_temp * 255),
-				Math.round(green_temp * 255),
-				Math.round(blue_temp * 255));
+		return new Color((float)red_temp, (float)green_temp, (float)blue_temp);
 	}
 	
-	private static void adjustRange(float color){
+	private static double adjustRange(double color){
 		if(color < 0){
 			color += 1;
 		}else if(color > 1){
 			color -= 1;
 		}
+		
+		return color;
 	}
 	
-	private static void runCorrectionTests(float color){
-		if(!correctionTest1(color) && !correctionTest2(color)
-				&& !correctionTest3(color)){
+	private static double runCorrectionTests(double color){
+		double color_temp = color;
+		
+		if(color_temp == correctionTest1(color) && color_temp == correctionTest2(color)
+				&& color_temp == correctionTest3(color)){
 			color = LL_MULTIPLIER;
 		}
+		
+		return color;
 	}
 	
-	private static boolean correctionTest1(float color){
-		if(6 * color < 1){
-			color = LL_MULTIPLIER + 
-					((LS_MULTIPLIER - LL_MULTIPLIER) * 6 * color);
-			return true;
-		}
-		return false;
+	private static double correctionTest1(double color){
+		return (6 * color < 1 ? LL_MULTIPLIER + 
+				((LS_MULTIPLIER - LL_MULTIPLIER) * 6 * color) : color);
 	}
 	
-	private static boolean correctionTest2(float color){
-		if(2 * color < 1){
-			color = LS_MULTIPLIER;
-			return true;
-		}
-		return false;
+	private static double correctionTest2(double color){
+		return (2 * color < 1 ? LS_MULTIPLIER : color);
 	}
 
-	private static boolean correctionTest3(float color){
-		if(3 * color < 2){
-			 color = LL_MULTIPLIER + ((LS_MULTIPLIER - LL_MULTIPLIER)
-					 * ((float)(2/3) - color) * 6);
-			 return true;
-		}
-		return false;
+	private static double correctionTest3(double color){
+		return (3 * color < 2 ? LL_MULTIPLIER + ((LS_MULTIPLIER - LL_MULTIPLIER)
+				 * (2 * CONST - color) * 6) : color);
+	}
+	
+	public static Color generateColorFromHue(int hue){
+		float hue_rad = (float)(hue) / (float)(360);
+		return Color.getHSBColor(hue_rad, (float)SATURATION, (float)VALUE);
 	}
 	
 }
