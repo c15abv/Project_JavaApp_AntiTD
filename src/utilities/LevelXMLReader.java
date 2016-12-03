e1514324648be822f5650c70f5b5185be244b6f4
JanNylen <jan.nylen92@gmail.com>
2016-12-03 12:08:19 +0100

    # WARNING: head commit changed in the meantime
    XMLfiler

--- src/utilities/LevelXMLReader.java
+++ src/utilities/LevelXMLReader.java
@@ -2,17 +2,35 @@ package utilities;
 
 import start.GameLevel;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
 public class LevelXMLReader{
-	
-	public LevelXMLReader(/*some xml file*/){
-	}
-	
-	/*private void load(){
-		
+    
+    public static void main(String[] args){
+        
+    }
+
+    public LevelXMLReader(/*some xml file*/){
+    }
+
+    public void readXML(){
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        
+        Document doc = builder.parse("");
+    }
+
+    /*private void load(){
+
 	}*/
-	
-	//
-	public GameLevel toLevel(){
-		return new GameLevel();
-	}
+
+
+
+    //
+    public GameLevel toLevel(){
+        return new GameLevel();
+    }
 }
