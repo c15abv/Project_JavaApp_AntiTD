75861f257dba15bfb974a92b082267b657ffcdb7
c15abv <c15abv@cs.umu.se>
2016-12-12 22:46:50 +0100

    asd

--- src/start/GameLevel.java
+++ src/start/GameLevel.java
@@ -20,7 +20,7 @@ public class GameLevel{
 	private int attackingPlayerScoreGoal = DEFAULT_PLAYER_SCORE_GOAL;
 	private int attackerCredit = DEFAULT_CREDIT;
 	private int defenderCredit = DEFAULT_CREDIT;
-	private int timeToFinish = DEFAULT_TIME;
+	private int timeToFinish = DEFAULT_TIME; 
 	
 	public GameLevel(){
 		this(DEFAULT_PLAYER_SCORE_GOAL, null, null, null);
@@ -40,6 +40,7 @@ public class GameLevel{
 			this.levelMap = new HashMap<AreaPosition, Tile>();
 		}
 	}
+	
 	
 	public void update(){
 		for(Map.Entry<AreaPosition, Tile> entry : levelMap.entrySet()){
